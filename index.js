527be4b6dd19d04bef67b36ea2813d73df9de1d9
shaun <ycorredius@gmail.com>
2020-02-15 16:37:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+const input = document.getElementById('input');
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
 }
